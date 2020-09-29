e36e0b4586be826ee91d28399fae6623d1a465d1
damiantopaz <damiantopaz@gmail.com>
2020-09-29 09:43:38 +0100

    redirection added

--- script.js
+++ script.js
@@ -1,39 +1,40 @@
 $(document).ready(function() {
-	$('#butsave').on('click', function() {
-		$("#butsave").attr("disabled", "disabled");
-		$("#loader").show();
-		var name = $('#name').val();
-		var email = $('#email').val();
-		var phone = $('#phone').val();
-		var city = $('#city').val();
-		if(name!="" && email!="" && phone!="" && city!=""){
-			$.ajax({
-				url: "save.php",
-				type: "POST",
-				data: {name: name,email: email,phone: phone,city: city},
-				cache: false,
+    $('#butsave').on('click', function() {
+        $("#butsave").attr("disabled", "disabled");
+        $("#loader").show();
+        var name = $('#name').val();
+        var email = $('#email').val();
+        var phone = $('#phone').val();
+        var city = $('#city').val();
+        if (name != "" && email != "" && phone != "" && city != "") {
+            $.ajax({
+                url: "save.php",
+                type: "POST",
+                data: { name: name, email: email, phone: phone, city: city },
+                cache: false,
 
-				success: function(dataResult){
-					var dataResult = JSON.parse(dataResult);
-					if(dataResult.statusCode==200){
-						$("#loader").hide(1000);
-						$("#butsave").removeAttr("disabled");
-						$('#fupForm').find('input:text').val('');
-						$("#success").show();
-						$('#success').html('Data added successfully !');
-						setTimeout(() => {
-						$('#success').hide();
-						}, 3000) 						
-					}
-					else if(dataResult.statusCode==400){alert("Error occured !");}
-					
-				}
-			});
-		}
-		else{
-			alert('Please fill all the field!');
-			 $("#butsave").removeAttr("disabled");
+                success: function(dataResult) {
+                    var dataResult = JSON.parse(dataResult);
+                    if (dataResult.statusCode == 200) {
+                        $("#loader").hide(1000);
+                        $("#butsave").removeAttr("disabled");
+                        $('#fupForm').find('input:text').val('');
+                        $("#success").show();
+                        $('#success').html('Data added successfully !');
+                        window.location.href = 'https://gmail.com';
+                        // setTimeout(() => {
+                        //     $('#success').hide();
+                        // }, 3000);
 
-		}
-	});
+                    } else if (dataResult.statusCode == 400) { alert("Error occured !"); }
+
+                }
+            });
+        } else {
+            alert('Please fill all the field!');
+            $("#butsave").removeAttr("disabled");
+
+        }
+    });
 });
+// });
